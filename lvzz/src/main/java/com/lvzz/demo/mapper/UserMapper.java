f531f9fd879d518bb86528b80da193c6f34f371f
sdau <5416830+syx226@user.noreply.gitee.com>
2022-01-21 16:29:20 +0800

    login

--- lvzz/src/main/java/com/lvzz/demo/mapper/UserMapper.java
+++ lvzz/src/main/java/com/lvzz/demo/mapper/UserMapper.java
@@ -3,5 +3,6 @@ package com.lvzz.demo.mapper;
 import com.lvzz.demo.entity.User;
 
 public interface UserMapper {
-    User queryUser();
+
+       User queryUser(String userName,String password);
 }
--- lvzz/src/main/java/com/lvzz/demo/service/Impl/UserServiceImpl.java
+++ lvzz/src/main/java/com/lvzz/demo/service/Impl/UserServiceImpl.java
@@ -1,15 +1,20 @@
 package com.lvzz.demo.service.Impl;
 
 import com.lvzz.demo.entity.User;
+import com.lvzz.demo.mapper.UserMapper;
 import com.lvzz.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public User queryUser(String userName, String password) {
-
-        return null;
+        User user = userMapper.queryUser(userName,password);
+        return user;
     }
 }
